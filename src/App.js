cb9644dfb252fa1f0ad57b5aa931448a33c62f68
Mirza Arslan <43127883+cdlschoolgit@users.noreply.github.com>
2026-01-13 11:22:55 +0500

    up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import AppRoutes from "./routes/Routes";
-
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
     <div className="App">
+      <Analytics />
       <AppRoutes />
     </div>
   );
